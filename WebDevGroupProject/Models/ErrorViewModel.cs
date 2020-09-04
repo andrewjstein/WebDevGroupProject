79d56090b32cff2ce27b0532f8a5e287627d5e52
andrew <andrew>
2020-09-04 16:34:59 -0400

    Add project files.

---
+++ WebDevGroupProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebDevGroupProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

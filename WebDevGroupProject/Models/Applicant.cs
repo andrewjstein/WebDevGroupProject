de4d6ff9ff36996a940e1f110004219aba8b011e
andrew <andrew>
2020-11-21 15:05:59 -0500

    Added applicant and ethnic origin models

---
+++ WebDevGroupProject/Models/Applicant.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebDevGroupProject.Models
+{
+    public class Applicant
+    {
+        public int ApplicantID { get; set; }
+
+        public string ApplicantFirstName { get; set; }
+
+        public string ApplicantLastName { get; set; }
+
+        public string ApplicantMiddleInitial { get; set; }
+
+        public string ApplicantCountry { get; set; }
+
+        public string ApplicantStreetAddress { get; set; }
+
+        public string ApplicantCity { get; set; }
+
+        public string ApplicantZip { get; set; }
+
+        public string ApplicantPhone { get; set; }
+
+        public string ApplicantEmail { get; set; }
+
+        public DateTime ApplicantBirthDate { get; set; }
+
+        public string ApplicantPrimaryCitizenship { get; set; }
+
+        public EthnicOrigin EthnicOrigin { get; set; }
+    }
+}
---
+++ WebDevGroupProject/Models/EthnicOrigin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebDevGroupProject.Models
+{
+    public class EthnicOrigin
+    {
+        public int EthnicOriginID { get; set; }
+
+        public string Description { get; set; }
+    }
+}

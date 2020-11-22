8398abbb7a1a300e8e1ec60910dee7efc839b034
andrew <andrew>
2020-11-21 20:44:42 -0500

    stein reference controller and view

---
+++ WebDevGroupProject/Controllers/ApplicationController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebDevGroupProject.ViewModels;
+
+namespace WebDevGroupProject.Controllers
+{
+    public class ApplicationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Reference()
+        {
+            var viewModel = new ReferenceViewModel();
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Reference(ReferenceViewModel viewModel)
+        {
+            return RedirectToAction("Index"); 
+        }
+    }
+}

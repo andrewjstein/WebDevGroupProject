b635a6fc6e7931793fcc536d64d165606393b746
Colin Dowd <colindowd@cedarville.edu>
2020-11-22 13:57:50 -0500

    Added Billing and Relationships View Modles

---
+++ WebDevGroupProject/ViewModels/BillingViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebDevGroupProject.ViewModels
+{
+    public class BillingViewModel
+    {
+        [Required]
+        public int CreditCardNumber { get; set; }
+        [Required]
+        public string BillingExpiration { get; set; }
+        [Required]
+        public string BillingCVV { get; set; }
+        [Required]
+        public string BillingName { get; set; }
+        [Required]
+        public string BillingAddress { get; set; }
+        [Required]
+        public string BillingCity { get; set; }
+        [Required]
+        public string BillingState { get; set; }
+        [Required]
+        public string BillingZip { get; set; }
+        [Required]
+        public string Signature { get; set; }
+    }
+}
---
+++ WebDevGroupProject/ViewModels/RelationshipsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using WebDevGroupProject.Models;
+
+namespace WebDevGroupProject.ViewModels
+{
+    public class RelationshipsViewModel
+    {
+        [Required]
+        public List<Guardian> Guardians { get; set; }
+        [Required]
+        public List<Sibling> Siblings { get; set; }
+        [Required]
+        public FamilyRole FamilyRole { get; set; }
+    }
+}

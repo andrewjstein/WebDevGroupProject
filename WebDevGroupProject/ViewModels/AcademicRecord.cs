7ef68c3afdc6fb959755a5e509e2f68e05fe7099
Andrew Spurr <andrewvspurr@cedarville.edu>
2020-11-22 16:24:25 -0500

    Addition of SpiritualBackGround viewModel

---
+++ WebDevGroupProject/ViewModels/AcademicRecord.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using WebDevGroupProject.Models;
+
+namespace WebDevGroupProject.ViewModels
+{
+    public class AcademicRecord
+    {
+        // Academic Record
+        [Required]
+        [Display(Name = "Name of High School")]
+        public string HighSchoolName { get; set; }
+        [Required]
+        [Display(Name = "CEEB Code")]
+        public int CEEBCode { get; set; }
+        [Required]
+        [Display(Name = "GPA")]
+        public int GPA { get; set; }
+        [Display(Name = "SAT")]
+        public int SAT { get; set; }
+        [Display(Name = "ACT")]
+        public int ACT { get; set; }
+        [Required]
+        [Display(Name = "High School Transcript")]
+        public string HighSchoolTranscriptPath { get; set; }
+        [Display(Name = "Past Universities")]
+        public List<PastUniversity> PastUniversities { get; set; }
+
+    }
+}
---
+++ WebDevGroupProject/ViewModels/SpiritualBackground.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebDevGroupProject.ViewModels
+{
+    public class SpiritualBackground
+    {
+        // Spiritual Background 
+        [Required]
+        [Display(Name = "Church Name")]
+        public string ChurchName { get; set; }
+        [Required]
+        [Display(Name = "Church Address")]
+        public string ChurchAddress { get; set; }
+        [Required]
+        [Display(Name = "Church City")]
+        public string ChurchCity { get; set; }
+        [Required]
+        [Display(Name = "Church State")]
+        public string ChurchState { get; set; }
+        [Required]
+        [Display(Name = "Church Zip")]
+        public string ChurchZip { get; set; }
+        [Required]
+        [Display(Name = "Pastor Name")]
+        public string PastorName { get; set; }
+        [Required]
+        [Display(Name = "Pastor Email")]
+        public string PastorEmail { get; set; }
+        [Required]
+        [Display(Name = "Profession of Faith")]
+        public bool ProfessionOfFaith { get; set; }
+        [Required]
+        [Display(Name = "Spiritual Journey")]
+        public string SpiritualJourney { get; set; }
+
+    }
+}
